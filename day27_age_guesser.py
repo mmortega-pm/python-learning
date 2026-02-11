60f375351fd747f27c523cdef98a7f2013ac1734
mmortega-pm <Whaazupdude@gmail.com>
2026-02-11 14:27:51 -0500

    Day 27: Dynamic API Requests

---
+++ day27_age_guesser.py
@@ -0,0 +1,24 @@
+import requests
+
+print("--- The AI Age Guesser ---")
+
+# 1. The Input (Dynamic)
+# We ask the user for a name, instead of hardcoding it
+name = input("Enter a name: ")
+
+# 2. The URL Construction
+# We inject the variable 'name' directly into the API address
+url = f"https://api.agify.io?name={name}"
+
+print(f"Checking the database for '{name}'...")
+
+# 3. The Fetch
+response = requests.get(url)
+data = response.json()
+
+# 4. The Prediction
+predicted_age = data['age']
+count = data['count']  # How many people provided data for this
+
+print(f"Based on {count} records...")
+print(f"I predict '{name}' is {predicted_age} years old.")
